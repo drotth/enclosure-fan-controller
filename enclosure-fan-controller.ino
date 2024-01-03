9a09b3f411ad8240eb7f83cfde44fdc6f85de01a
Andreas Drotth <drotth@hotmail.com>
2024-01-03 01:55:56 +0100

    Initial commit

---
+++ enclosure-fan-controller.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin (115200);
+  while (!Serial);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String inputString = Serial.readString();
+    Serial.println (inputString);
+  }
+}
